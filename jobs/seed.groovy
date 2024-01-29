693e60bc94671a5cc13a58bd076559c045960fa2
meta <metacringer@metacringer-pc.localdomain>
2024-01-29 20:24:02 +0200

    seed job

--- jobs/seed.groovy
+++ jobs/seed.groovy
@@ -1,18 +1,25 @@
 def projects = ["realworld-spring-boot-native", "MASTG-Hacking-Playground", "flask-realworld-example-app"]
 def owner = "MetaCringer"
-folder("jobs")
-def project=projects[0]
-pipelineJob("jobs/${project}") {
-    // parameters {
-    // }
-    definition {
-        cpsScm {
-            
-            scm {
-                git("https://github.com/${owner}/${project}.git")
+
+folder('jobs') {
+    displayName('jobs')
+}
+for (def project in projects){
+
+    
+    pipelineJob("jobs/${project}") {
+        parameters {
+
+        }
+        definition {
+            cpsScm {
+                
+                scm {
+                    git("https://github.com/${owner}/${project}.git")
+                }
+                scriptPath("Jenkinsfile")
+                lightweight(true)
             }
-            scriptPath("Jenkinsfile")
-            lightweight(true)
         }
     }
 }
